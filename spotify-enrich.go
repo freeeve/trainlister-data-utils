1e4eaa985d3d335ddeaa24bc2eb76c976e2d432c
Wes Freeman <wes@skeweredrook.com>
2014-10-22 14:09:20 -0400

    adding album img, url, etc.

--- spotify-enrich.go
+++ spotify-enrich.go
@@ -20,8 +20,21 @@ type TrackResponse struct {
 }
 
 type Track struct {
-	Duration int    `json:"duration_ms"`
-	Id       string `json:"id"`
+	Duration   int    `json:"duration_ms"`
+	Id         string `json:"id"`
+	PreviewURL string `json:"preview_url"`
+	Album
+}
+
+type Album struct {
+	Name   string  `json:"name"`
+	Images []Image `json:"images"`
+}
+
+type Image struct {
+	Height int    `json:"width"`
+	Width  int    `json:"width"`
+	Url    string `json:"url"`
 }
 
 func main() {
@@ -64,9 +77,20 @@ func main() {
 		}
 
 		if len(r.Tracks.Items) > 0 {
-			fmt.Println(r.Tracks.Items[0])
+			track = r.Tracks.Items[0]
 			// update the db
-			_, err := db.Exec("MATCH (t:Track {name:{0}, artist:{1}}) SET t.duration = {2}, t.trackId = {3}", name, artist, r.Tracks.Items[0].Duration, r.Tracks.Items[0].Id)
+			imgUrl := ""
+			if len(r.Tracks.Items[0].Album.Images) > 0 {
+				r.Tracks.Items[0].Album.Images[0].Url
+			}
+			_, err := db.Exec(`
+			MATCH (t:Track {name:{0}, artist:{1}}) 
+			  SET t.duration = {2}, 
+			      t.trackId = {3},
+			      t.previewUrl = {4},
+			      t.album = {5},
+			      t.albumImageUrl = {6}
+			  `, name, artist, track.Duration, track.Id, track.PreviewURL, track.Album.Name, imgUrl)
 			if err != nil {
 				log.Fatal(err)
 			}
